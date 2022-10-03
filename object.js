b0f0fcfb82939837c5dc8c946560404b961aefb2
MuhammadZikri <zikrim577@gmail.com>
2022-10-03 11:22:53 +0700

    Tipe Data Object

---
+++ object.js
@@ -0,0 +1,14 @@
+const biodata = {
+    "nama_lengkap": "alexander graham bell",
+    umur: 75,
+    lahir: "Skotlandia",
+
+    lulusan: "Harvard University",
+    penelitian: "Penemu Telepon",
+};
+
+console.info(`Nama Lengkap : ${biodata["nama_lengkap"]}`);
+console.info(`Umur : ${biodata.umur}`);
+console.info(`Lahir : ${biodata.lahir}`);
+console.info(`Lulusan : ${biodata.lulusan}`);
+console.info(`Penelitian : ${biodata.penelitian}`);
